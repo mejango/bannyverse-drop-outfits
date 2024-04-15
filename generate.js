92e78ec50a7f9e447bacb52e509b7b3ae2c6e5db
mejango <mejango@protonmail.com>
2024-04-14 23:14:08 -0300

    updated assets. keccak is only content body

--- generate.js
+++ generate.js
@@ -4,6 +4,7 @@ const fs = require('fs').promises;
 const path = require('path');
 const { exec } = require('child_process');
 const { keccak256 } = require('js-sha3');
+const { optimize } = require('svgo');
 const BASE = require('base-x');
 const BASE58 = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz';
 const base58 = BASE(BASE58);
@@ -12,9 +13,11 @@ const execPromise = util.promisify(exec);
 
 const baseFolder = process.argv[2]; // Base folder path from command line argument
 const svgFolder = path.join(baseFolder, 'svgs');
+const optimizedSvgFolder = path.join(baseFolder, 'optimized-svgs');
 const hashesFilePath = path.join(baseFolder, 'hashes.txt');
 const encodedHashesFilePath = path.join(baseFolder, 'encoded-hashes.txt');
 const keccakHashesFilePath = path.join(baseFolder, 'keccak-hashes.txt'); // Path for Keccak hashes file
+const prettyFilePath = path.join(baseFolder, 'pretty.txt'); // Viewer
 
 function encodeIpfsUri(cid) {
     return '0x' + Buffer.from(base58.decode(cid).slice(2)).toString('hex');
@@ -34,8 +37,24 @@ async function uploadFiles(files) {
     for (const file of files) { 
         if (path.extname(file) !== '.svg') continue;
         const filePath = path.join(svgFolder, file);
-        console.log(`Uploading ${filePath}...`);
-        const curlCommand = `curl -X POST -F file=@${filePath} ` +
+        const content = await fs.readFile(filePath);
+        console.log(`Optimizing ${filePath}...`);
+        const optimizedContent = optimize(content, {
+          multipass: true,
+          plugins: [
+            {
+              name: 'preset-default',
+            },
+          ],
+          js2svg: {
+            indent: 0,  
+            pretty: false
+          }
+        }).data;
+        await fs.writeFile(path.join(optimizedSvgFolder, file), optimizedContent);
+        const optimizedFilePath = path.join(optimizedSvgFolder, file);
+        console.log(`Uploading ${optimizedFilePath}...`);
+        const curlCommand = `curl -X POST -F file=@${optimizedFilePath} ` +
                             `-u "${process.env.IPFS_KEY}:${process.env.IPFS_SECRET}" ` +
                             `"https://ipfs.infura.io:5001/api/v0/add"`;
 
@@ -49,10 +68,16 @@ async function uploadFiles(files) {
                 const encodedHash = encodeIpfsUri(response.Hash);
                 await fs.appendFile(encodedHashesFilePath, encodedHash + '\n');
                 console.log(`Encoded hash ${encodedHash} written to ${encodedHashesFilePath}`);
-                const content = await fs.readFile(filePath);
-                const keccakHash = keccak256(content);
+                // Remove leading and trailing svg tag.
+                const optimizedContentBody = optimizedContent.replace(/^<svg[^>]*>|<\/svg>$/g, ''); 
+                const keccakHash = '0x' + keccak256(optimizedContentBody);
                 await fs.appendFile(keccakHashesFilePath, keccakHash + '\n');
                 console.log(`Keccak hash for ${file} written to file: ${keccakHash}`);
+                await fs.appendFile(prettyFilePath, `${file} \n`);
+                await fs.appendFile(prettyFilePath, `IPFS uri: ${response.Hash} \n`);
+                await fs.appendFile(prettyFilePath, `Encoded IPFS uri: ${encodedHash} \n`);
+                await fs.appendFile(prettyFilePath, `SVG keccak hash: ${keccakHash} \n`);
+                await fs.appendFile(prettyFilePath, '\n');
             } else {
                 console.log(`No Hash found in response for ${file}`);
             }
@@ -73,6 +98,7 @@ async function main() {
         await clearOrCreateFile(hashesFilePath);
         await clearOrCreateFile(encodedHashesFilePath);
         await clearOrCreateFile(keccakHashesFilePath); 
+        await clearOrCreateFile(prettyFilePath); 
 
         const files = await fs.readdir(svgFolder);
         await uploadFiles(files);
